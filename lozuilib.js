6c064f91733774419bbe9bf4a386997951e1783e
Oqarshi <94034118+Oqarshi@users.noreply.github.com>
2023-06-24 21:06:47 -0700

    Update lozuilib.js

--- lozuilib.js
+++ lozuilib.js
@@ -9,7 +9,7 @@ lozData.next     = 0; // next board id.
 lozData.hashFull = 0;
 lozData.mvNum    = 0;
 lozData.mvStr    = '';
-lozData.source   = 'lozza.js';
+lozData.source   = 'newengine1.js';
 
 //{{{  Number.round
 
